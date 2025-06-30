1515cf30f0e74e1bba9929a283c536812426ac2b
unknown <qaiserwaheed00@gmail.com>
2025-07-01 04:20:30 +0500

    auth pages completed

--- Candice-Client/src/features/auth/Login.tsx
+++ Candice-Client/src/features/auth/Login.tsx
@@ -1,31 +1,75 @@
-import { Card, Stack, Text } from "@mantine/core";
+import {
+  Anchor,
+  Button,
+  Card,
+  Checkbox,
+  Divider,
+  Group,
+  Input,
+  Stack,
+  Text,
+} from "@mantine/core";
 
 const LoginPage = () => {
   return (
     <Card
       style={{
-        border: "1px solid",
+        border: "1px solid #C5C5C5",
       }}
     >
-      <Stack>
-        <Stack>
+      <Stack m={24}>
+        <Stack justify="center" display={"flex"} align="center">
           <Text
             size="xl"
             fw={900}
             variant="gradient"
             gradient={{ from: "#A955F5", to: "#EA489B", deg: 90 }}
           >
-            Gradient Text
+            MBMM Cafe
           </Text>
-          <Stack mt={"30px"}>
-            <Text size="xl" fz={"h1"} fw={"bold"}>
-              Welcome Back
-            </Text>
-            <Text size="lg" c={"#676870"} fz={"h1"}>
-              Sign in to your account to continue
-            </Text>
-          </Stack>
         </Stack>
+        <Stack mt={"30px"} justify="cemter " align="center" gap={1} mb={"8px"}>
+          <Text size="xl" fz={"h1"} fw={"bold"}>
+            Welcome Back
+          </Text>
+          <Text size="md" c={"#676870"}>
+            Sign in to your account to continue
+          </Text>
+        </Stack>
+
+        <Stack gap={10}>
+          <Text fz={"sm"} c={"#676870"}>
+            Email
+          </Text>
+          <Input placeholder="Emter Your Email" w={"400px"} h={"40px"}></Input>
+        </Stack>
+        <Stack gap={10}>
+          <Text fz={"sm"} c={"#676870"}>
+            Password
+          </Text>
+          <Input
+            placeholder="Enter Your Password"
+            w={"400px"}
+            h={"40px"}
+          ></Input>
+        </Stack>
+        <Group justify="space-between">
+          <Checkbox label="Remember me" c={"gray"}></Checkbox>
+          <Anchor c={"#D6B588"}>Forgot Password?</Anchor>
+        </Group>
+        <Button color="#D6B588" size="md">
+          Sign In
+        </Button>
+        <Divider my="md" />
+        <Button color="#D6B588" variant="default" c={"gray"} size="md">
+          Or Continue with Google
+        </Button>
+        <Group justify="center">
+          <Text>
+            Don't have an account?
+            <Anchor c="#D6B588"> Create Account</Anchor>
+          </Text>
+        </Group>
       </Stack>
     </Card>
   );
--- Candice-Client/src/features/auth/Signup.tsx
+++ Candice-Client/src/features/auth/Signup.tsx
@@ -1,4 +1,97 @@
+import {
+  Anchor,
+  Button,
+  Card,
+  Divider,
+  Group,
+  Input,
+  Stack,
+  Text,
+} from "@mantine/core";
+
 const SignupPage = () => {
-  return <>hasdasdasdello</>;
+  return (
+    <Card
+      style={{
+        border: "1px solid #C5C5C5",
+      }}
+    >
+      <Stack m={24}>
+        <Stack justify="center" display={"flex"} align="center">
+          <Text
+            size="xl"
+            fw={900}
+            variant="gradient"
+            gradient={{ from: "#A955F5", to: "#EA489B", deg: 90 }}
+          >
+            MBMM Cafe
+          </Text>
+
+          <Text size="md" c={"#676870"}>
+            Create an account to continue
+          </Text>
+        </Stack>
+
+        <Group>
+          <Stack gap={10}>
+            <Text fz={"sm"} c={"#676870"}>
+              First Name
+            </Text>
+            <Input placeholder="Emter Your Email"></Input>
+          </Stack>
+          <Stack gap={10}>
+            <Text fz={"sm"} c={"#676870"}>
+              Last Name
+            </Text>
+            <Input placeholder="Emter Your Email"></Input>
+          </Stack>
+        </Group>
+
+        <Stack gap={10}>
+          <Text fz={"sm"} c={"#676870"}>
+            Email
+          </Text>
+          <Input placeholder="Emter Your Email" w={"400px"} h={"40px"}></Input>
+        </Stack>
+        <Stack gap={10}>
+          <Text fz={"sm"} c={"#676870"}>
+            Assigned Coach Email
+          </Text>
+          <Input
+            placeholder="Enter Your Password"
+            w={"400px"}
+            h={"40px"}
+          ></Input>
+        </Stack>
+        <Stack gap={10}>
+          <Text fz={"sm"} c={"#676870"}>
+            Passowrd
+          </Text>
+          <Input placeholder="Confirm Password" w={"400px"} h={"40px"}></Input>
+        </Stack>
+        <Stack gap={10}>
+          <Text fz={"sm"} c={"#676870"}>
+            Confirm Password
+          </Text>
+          <Input placeholder="Confirm Password" w={"400px"} h={"40px"}></Input>
+        </Stack>
+
+        <Button color="#D6B588" size="md">
+          Sign Up
+        </Button>
+        <Divider my="md" />
+        <Button color="#D6B588" variant="default" c={"gray"} size="md">
+          Or Continue with Google
+        </Button>
+        <Group justify="center">
+          <Text>
+            Don't have an account?
+            <Anchor c="#D6B588"> Create Account</Anchor>
+          </Text>
+        </Group>
+      </Stack>
+    </Card>
+  );
 };
+
 export default SignupPage;
